669f9b612fa40623abb9d3a45f407e9343bb6627
Ankit Rai <a2.ankitrai@gmail.com>
2022-12-01 09:11:59 +0100

    initial commit

---
+++ hello/popup.js
@@ -0,0 +1,6 @@
+console.log("This is a popup!")
+const d = document.getElementById("date")
+const currentDate = new Date()
+console.log("This is a another popup!")
+// d.innerHTML = currentDate.toLocaleDateString().toString()
+ d.innerHTML = "abra ca dabra";
